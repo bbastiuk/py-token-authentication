c673a2558dacdb41f686a5c0fe15d2c840d31a19
Bohdan Bastiuk <bbastyuk@gmail.com>
2025-02-16 15:24:14 +0100

    Solution 2

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,10 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
-            return True
+            return bool(request.user and request.user.is_authenticated)
         return bool(request.user and request.user.is_staff)
+
 
 class IsAdminOrIfAuthenticatedListCreate(BasePermission):
     def has_permission(self, request, view):
@@ -13,6 +15,7 @@ class IsAdminOrIfAuthenticatedListCreate(BasePermission):
         if request.method == "POST":
             return bool(request.user and request.user.is_staff)
         return False
+
 
 class IsAdminOrIfAuthenticatedCRUD(BasePermission):
     def has_permission(self, request, view):
--- cinema/views.py
+++ cinema/views.py
@@ -4,11 +4,18 @@ from django.utils.timezone import make_aware
 from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order,
+)
 from cinema.serializers import (
     GenreSerializer,
     ActorSerializer,
@@ -20,54 +27,63 @@ from cinema.serializers import (
     MovieSessionListSerializer,
     MovieSessionDetailSerializer,
 )
-from cinema.permissions import (
-    IsAdminOrIfAuthenticatedReadOnly,
-    IsAdminOrIfAuthenticatedCRUD,
-)
+from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 
-class GenreViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+
+class GenreViewSet(GenericViewSet, mixins.ListModelMixin,
+                   mixins.CreateModelMixin):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, IsAdminOrIfAuthenticatedReadOnly)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
-class ActorViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+
+class ActorViewSet(GenericViewSet, mixins.ListModelMixin,
+                   mixins.CreateModelMixin):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, IsAdminOrIfAuthenticatedReadOnly)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
-class CinemaHallViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+
+class CinemaHallViewSet(GenericViewSet, mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, IsAdminOrIfAuthenticatedReadOnly)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
-class MovieViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin):
+
+class MovieViewSet(GenericViewSet, mixins.ListModelMixin,
+                   mixins.CreateModelMixin, mixins.RetrieveModelMixin):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, IsAdminOrIfAuthenticatedReadOnly)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_serializer_class(self):
         if self.action == "retrieve":
             return MovieDetailSerializer
         return MovieSerializer
 
-class MovieSessionViewSet(
-    GenericViewSet,
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-):
-    queryset = MovieSession.objects.all().select_related("movie", "cinema_hall").annotate(
-        tickets_available=F("cinema_hall__rows") * F("cinema_hall__seats_in_row") - Count("tickets")
+
+class MovieSessionViewSet(GenericViewSet, mixins.ListModelMixin,
+                          mixins.CreateModelMixin, mixins.RetrieveModelMixin,
+                          mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = (
+        MovieSession.objects.all()
+        .select_related("movie", "cinema_hall")
+        .annotate(
+            tickets_available=(
+                F("cinema_hall__rows") *
+                F("cinema_hall__seats_in_row") -
+                Count("tickets")
+            )
+        )
     )
     serializer_class = MovieSessionSerializer
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, IsAdminOrIfAuthenticatedCRUD)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
         qs = self.queryset
@@ -87,12 +103,16 @@ class MovieSessionViewSet(
             return MovieSessionDetailSerializer
         return MovieSessionSerializer
 
+
 class OrderPagination(PageNumberPagination):
     max_page_size = 100
 
-class OrderViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+
+class OrderViewSet(GenericViewSet, mixins.ListModelMixin,
+                   mixins.CreateModelMixin):
     queryset = Order.objects.prefetch_related(
-        "tickets__movie_session__movie", "tickets__movie_session__cinema_hall"
+        "tickets__movie_session__movie",
+        "tickets__movie_session__cinema_hall",
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
@@ -100,9 +120,14 @@ class OrderViewSet(GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixi
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Order.objects.none()
         return Order.objects.filter(user=self.request.user)
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
-        return Response({"count": len(serializer.data), "results": serializer.data})
+        return Response({
+            "count": len(serializer.data),
+            "results": serializer.data,
+        })
